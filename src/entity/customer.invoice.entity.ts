32a16a1fbb045aeda1fe903ce5a933c7be37035a
JtMe <johnnatanmartinez@yahoo.com>
2020-08-05 10:02:15 -0500

    cambio de nombre de entitades

---
+++ src/entity/customer.invoice.entity.ts
@@ -0,0 +1,20 @@
+import {Entity, Column, PrimaryGeneratedColumn, ManyToOne, OneToMany} from 'typeorm';
+import {Customer} from './customer.entity';
+import { SaleInvoiceDescription } from './sales.invoice.description.entity';
+
+@Entity()
+export class CustomerInvoice {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @ManyToOne(type => Customer, customer => customer.customerInvoice)
+    customer: Customer;
+
+    @Column()
+    dateCreation: Date;
+    
+    @OneToMany(type => SaleInvoiceDescription, saleInvoiceDescription => saleInvoiceDescription.customerInvoice)
+    saleInvoiceDescription: SaleInvoiceDescription[];
+
+}
---
+++ src/entity/supplier.invoice.entity.ts
@@ -0,0 +1,20 @@
+import {Entity, Column, PrimaryGeneratedColumn, ManyToOne, OneToMany} from 'typeorm';
+import {Supplier} from './supplier.entity';
+import { PurchaseInvoiceDescription } from './purchase.invoice.description.entity';
+
+@Entity()
+export class SupplierInvoice {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @ManyToOne(type => Supplier, supplier => supplier.supplierInvoice)
+    supplier: Supplier;
+
+    @Column()
+    dateCreation: Date;
+
+    @OneToMany(type => PurchaseInvoiceDescription, purchaseInvoiceDescription => purchaseInvoiceDescription.supplierInvoice)
+    purchaseInvoiceDescription: PurchaseInvoiceDescription[];
+
+}
