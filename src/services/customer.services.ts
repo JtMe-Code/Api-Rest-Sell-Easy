c8b741a3d494233b280861666e72710082e2a2c7
JtMe <johnnatanmartinez@yahoo.com>
2020-08-05 11:07:42 -0500

    creando servicios de customer

--- src/services/customer.services.ts
+++ src/services/customer.services.ts
@@ -1,3 +1,32 @@
 import {getRepository} from 'typeorm';
 import {Customer} from '../entity/customer.entity';
 import {ICustomer} from '../interfaces/customer';
+
+// *CRU -D*
+
+export class CustomerService {
+    private customer = getRepository(Customer);
+    constructor(private requestBody: ICustomer) {
+        
+    }
+    
+    async create():Promise<string | ICustomer>{
+        const result = await this.customer.find({
+            where: {typeIdentification: this.requestBody.typeIdentification, identification: this.requestBody.identification}
+        });
+        if(result){
+            return `Ya existe un cliente con ${this.requestBody.typeIdentification} ${this.requestBody.identification}`;
+        }
+
+        const saveData = await this.customer.save(this.requestBody);
+        return saveData;
+    }
+
+    async read(){
+
+    }
+
+    async update(){
+        
+    }
+}
--- src/services/login.services.ts
+++ src/services/login.services.ts
@@ -4,24 +4,25 @@ import {ILogin} from '../interfaces/login';
 import {Encrypt} from './helpers/encrypt';
 import {JWT} from './helpers/jwt';
 
-export class LoginServices {    
+export class LoginServices {
+    private login = getRepository(Login);
     constructor(private requestBody: ILogin){
     }
 
     async signup(): Promise<string | ILogin>{
-        const result = await getRepository(Login).findOne({user: this.requestBody.user});
+        const result = await this.login.findOne({user: this.requestBody.user});
         if(result){
             return "El usuario ya existe";
         }
         const encryptedPassword = await new Encrypt(this.requestBody.password).encryptedPassword();
         this.requestBody.password = encryptedPassword;
 
-        const saveResult = await getRepository(Login).save(this.requestBody);
-        return saveResult;
+        const saveData = await this.login.save(this.requestBody);
+        return saveData;
     }
 
     async signin(): Promise<string | {token: string}>{
-        const result = await getRepository(Login).findOne({user: this.requestBody.user});
+        const result = await this.login.findOne({user: this.requestBody.user});
         if(!result){
             return "Usuario o contraseña incorrecta";
         }
