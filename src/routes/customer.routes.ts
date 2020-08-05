9ca47d46b216f10573458ec2fdf2e6033cd68deb
JtMe <johnnatanmartinez@yahoo.com>
2020-08-05 15:31:57 -0500

    actualizacion de cutomer routes

--- src/routes/customer.routes.ts
+++ src/routes/customer.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import { CustomerControllers } from '../controllers/customer.controllers';
 
 const router = Router();
 
-router.get('/customer', (req, res)=>{
-    res.json({hola: "mundo"})
-});
+router.post('/customer', CustomerControllers.create);
+router.post('/customer/:id', CustomerControllers.read);
+router.post('/customer/all', CustomerControllers.readAll);
+router.put('/customer/:id', CustomerControllers.update);
 
 export default router;
